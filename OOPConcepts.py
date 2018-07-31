d7a5f7429b626c3a74c7f2afc517dd9e1cc130bb
Kshitija Murudi <kmurudi@ncsu.edu>
2018-07-31 10:55:44 -0700

    OOP basics

---
+++ OOPConcepts.py
@@ -0,0 +1,40 @@
+# A program to create different animals
+
+class Animal:
+
+    def __init__(self, name, color):
+        self.name = name
+        self.color = color
+
+    def sound(self):
+        print(self.name+" is speaking normal")
+
+# Demonstrating Inheritance from parent class - Animal
+class Dog(Animal):
+    # See how you can inherit parent class attributes without defining them here in child class
+    def describe(self):
+        print("Hi I am "+self.name+", a dog! of color- "+self.color)
+
+    # Concept of Overriding methods of parent class
+    def sound(self):
+        print("Bark Bark.")
+
+
+class Cat(Animal):
+    # See how you can inherit parent class attributes without defining them here in child class
+    def describe(self):
+        print("Hi I am " + self.name + ", a cat! of color- " + self.color)
+
+    # Concept of Overriding methods of parent class
+    def sound(self):
+        print("Meow Meow.")
+
+# Creating objects - class instances
+dog = Dog("coco","black")
+cat = Cat("kitty","white")
+
+# Testing the methods created now
+dog.describe()
+dog.sound()
+cat.describe()
+cat.sound()
